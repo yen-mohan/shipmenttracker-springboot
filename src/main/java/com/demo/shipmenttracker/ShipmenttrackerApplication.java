16308a182566b843d711dbeec732de8b47c8437a
yen-mohan <yen.mohan@gmail.com>
2025-07-10 10:40:33 -0700

    Initial Spring Boot project commit

---
+++ src/main/java/com/demo/shipmenttracker/ShipmenttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.shipmenttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmenttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmenttrackerApplication.class, args);
+	}
+
+}
